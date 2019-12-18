23bc8a041bbee565947ed55dbf849ef0a4112237
Matt Illies <mattillies19@gmail.com>
2019-12-18 16:22:08 -0600

    changed bottom text

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-
 //Sticky Nav Bar
-const nav = document.querySelector("#main");
+const nav = document.querySelector('#main');
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-    if(window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
-    } else {
-        document.body.classList.remove('fixed-nav');
-        document.body.style.paddingTop = 0;
-    }
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
 }
 
 window.addEventListener('scroll', fixNav);
-
 
 //Project Panel Transition
 const panels = document.querySelectorAll('.panel');
 
 function addOpen() {
-    this.classList.add('open');
+  this.classList.add('open');
 }
 
 function addActive() {
-        this.classList.add('open-active');
+  this.classList.add('open-active');
 }
 
 function removeOpen() {
-    this.classList.remove('open');
+  this.classList.remove('open');
 }
 
 function removeActive() {
-        this.classList.remove('open-active');
+  this.classList.remove('open-active');
 }
-
 
 panels.forEach(panel => panel.addEventListener('mouseenter', addOpen));
 panels.forEach(panel => panel.addEventListener('mouseleave', removeOpen));
